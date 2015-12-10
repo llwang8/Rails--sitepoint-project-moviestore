75c621b4872069adbbb6d43c76759874c5962409
llwang8 <wangxbl56@gmail.com>
2015-12-10 16:09:38 -0500

    add devise user model(authentication)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :movies, only: [:show, :index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -6,6 +7,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'movies#index'
+
+  #devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
