2b72568c15713dde61b74a05e8228e50282c21ed
llwang8 <wangxbl56@gmail.com>
2015-12-13 21:16:10 -0500

    fix some errors

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,19 +1,13 @@
 class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+
   def show
-    @movies = Movie.find(params[:id])
+    @movie = Movie.find(params[:id])
     @cart_action = @movie.cart_action current_user.try :id
   end
 
-  def index
-    @movie = Movie.all
-  end
-
-  def poster
-    "http://ia.media-imdb.com/images/M/#{poster_url}"
-  end
-
-  def imdb
-    "http://www.imdb.com/title/#{imdb_id}/"
-  end
-
 end
+
